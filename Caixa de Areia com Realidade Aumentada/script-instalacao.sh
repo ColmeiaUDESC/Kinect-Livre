3c1c889a4a070acb0f59f650770ad6ce5d7cfc50
Mark <56490702+marcosoft47@users.noreply.github.com>
2025-04-16 16:28:15 -0300

    consertado erro quando rodava com root

--- Caixa de Areia com Realidade Aumentada/script-instalacao.sh
+++ Caixa de Areia com Realidade Aumentada/script-instalacao.sh
@@ -2,7 +2,7 @@ if [ "$EUID" -ne 0 ]
   then echo "Por favor, execute como root (sudo script-instalacao.sh)"
   exit
 fi
-
+USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 git clone https://github.com/ColmeiaUDESC/Kinect-Livre.git
 
 cd Kinect-Livre/Caixa\ de\ Areia\ com\ Realidade\ Aumentada/arquivos/biblioteca
@@ -13,7 +13,7 @@ sudo dpkg -i libdc1394-22_2.2.5-1_amd64.deb
 cd ..
 bash Build-Ubuntu-MOD.sh
 
-cd ~/src
+cd $USER_HOME/src
 wget http://web.cs.ucdavis.edu/~okreylos/ResDev/Kinect/Kinect-3.10.tar.gz
 tar -xfz Kinect-3.10.tar.gz
 cd Kinect-3.10
@@ -21,7 +21,7 @@ make
 sudo make install
 sudo make installudevrules
 
-cd ~/src
+cd $USER_HOME/src
 curl -L "https://avatars.githubusercontent.com/u/54866625?s=400&u=184d63b6c7ecc161f9ebbad8f6e7b32b2e600253&v=4" -o colmeia.jpg
 cd SARndbox-2.8
 touch RunSARndbox.sh
@@ -29,7 +29,7 @@ printf "#!/bin/bash\n
 cd ~/src/SARndbox-2.8\n
 ./bin/SARndbox -uhm -fpv" >> RunSARndbox.sh
 
-chmod a+x ~/src/SARndbox-2.8/RunSARndbox.sh
+chmod a+x $USER_HOME/src/SARndbox-2.8/RunSARndbox.sh
 cd ~/Desktop/
 touch Caixa\ de\ Areia.desktop
 printf "
