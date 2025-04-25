345524fd795abcd72eadd4cb1a78d9fb58eb3b59
Mark <56490702+marcosoft47@users.noreply.github.com>
2025-04-25 17:40:01 -0300

    atualizado script de instalação, ainda não funcionando

--- Caixa de Areia com Realidade Aumentada/script-instalacao.sh
+++ Caixa de Areia com Realidade Aumentada/script-instalacao.sh
@@ -1,27 +1,29 @@
-if [ "$EUID" -ne 0 ]
-  then echo "Por favor, execute como root (sudo script-instalacao.sh)"
-  exit
-fi
-USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+# if [ "$EUID" -ne 0 ]
+#   then echo "Por favor, execute como root (sudo script-instalacao.sh)"
+#   exit
+# fi
+# USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+sudo echo "Agora espere"
 git clone https://github.com/ColmeiaUDESC/Kinect-Livre.git
 
 cd Kinect-Livre/Caixa\ de\ Areia\ com\ Realidade\ Aumentada/arquivos/biblioteca
 
+sudo apt install libraw
 sudo dpkg -i libdc1394-22-dev_2.2.5-1_amd64.deb
 sudo dpkg -i libdc1394-22_2.2.5-1_amd64.deb
 
 cd ..
 bash Build-Ubuntu-MOD.sh
 
-cd $USER_HOME/src
+cd $HOME/src
 wget http://web.cs.ucdavis.edu/~okreylos/ResDev/Kinect/Kinect-3.10.tar.gz
-tar -xfz Kinect-3.10.tar.gz
+tar -xzf Kinect-3.10.tar.gz
 cd Kinect-3.10
 make
 sudo make install
 sudo make installudevrules
 
-cd $USER_HOME/src
+cd ..
 curl -L "https://avatars.githubusercontent.com/u/54866625?s=400&u=184d63b6c7ecc161f9ebbad8f6e7b32b2e600253&v=4" -o colmeia.jpg
 cd SARndbox-2.8
 touch RunSARndbox.sh
@@ -29,15 +31,7 @@ printf "#!/bin/bash\n
 cd ~/src/SARndbox-2.8\n
 ./bin/SARndbox -uhm -fpv" >> RunSARndbox.sh
 
-chmod a+x $USER_HOME/src/SARndbox-2.8/RunSARndbox.sh
-cd $USER_HOME/Desktop/
+chmod a+x $HOME/src/SARndbox-2.8/RunSARndbox.sh
+cd $HOME/Desktop/
 touch Caixa\ de\ Areia.desktop
-printf "
-[Desktop Entry]
-Version=1.0
-Type=Application
-Terminal=false
-Icon=~/src/colmeia.png
-Exec=~/src/SARndbox-2.8/RunSARndbox.sh
-Name=Começa a Caixa de Areia
-Comment=" >> 
+printf "[Desktop Entry]\nVersion=1.0\nType=Application\nTerminal=false\nIcon=~/src/colmeia.png\n\nExec=~/src/SARndbox-2.8/RunSARndbox.sh\nName=Começa a Caixa de Areia\nComment=" >> Caixa\ de\ Areia.desktop
